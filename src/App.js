c6dfcc9028f0ad6bf5a508540b7c0422f0c563e8
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-07 16:33:33 +0000

    adding 6 shows

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import logo from './pulpFiction.jpg';
+import tvShow1 from './black-mirror-tv-show.jpg';
+import tvShow2 from './BreakingBad.jpg';
+import tvShow3 from './lightdeathnoteposter.jpg';
+import tvShow4 from './GameOfThrones.jpg';
+import tvShow5 from './The-Walking-Dead-Season-3.jpg';
+import tvShow6 from './TheWire.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +13,28 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="cover" alt="logo" />
+        <div class="row">
+          <div class="column">
+            <img src={tvShow1} className="TV"/>
+          </div>
+          <div class="column">
+            <img src={tvShow2} className="TV"/>
+          </div>
+          <div class="column">
+            <img src={tvShow3} className="TV"/>
+          </div>
+        </div>
+   <div class="row">
+          <div class="column">
+            <img src={tvShow4} className="TV"/>
+          </div>
+          <div class="column">
+            <img src={tvShow5} className="TV"/>
+          </div>
+          <div class="column">
+            <img src={tvShow6} className="TV"/>
+          </div>
+        </div>
         </header>
       </div>
     );
