282b3a56353322259e906e4ff3a0e5c8607ee434
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-08 12:24:04 +0000

    Replace TV show titles with covers #2 & cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './pulpFiction.jpg';
 import tvShow1 from './black-mirror-tv-show.jpg';
 import tvShow2 from './BreakingBad.jpg';
 import tvShow3 from './lightdeathnoteposter.jpg';
@@ -12,30 +11,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
         <div class="row">
           <div class="column">
-            <img src={tvShow1} className="TV"/>
+            <img src={tvShow1} className="TV" alt='' />
           </div>
           <div class="column">
-            <img src={tvShow2} className="TV"/>
+            <img src={tvShow2} className="TV" alt='' />
           </div>
           <div class="column">
-            <img src={tvShow3} className="TV"/>
+            <img src={tvShow3} className="TV" alt='' />
           </div>
         </div>
-   <div class="row">
+        <div class="row">
           <div class="column">
-            <img src={tvShow4} className="TV"/>
+            <img src={tvShow4} className="TV" alt='' />
           </div>
           <div class="column">
-            <img src={tvShow5} className="TV"/>
+            <img src={tvShow5} className="TV" alt='' />
           </div>
           <div class="column">
-            <img src={tvShow6} className="TV"/>
+            <img src={tvShow6} className="TV" alt='' />
           </div>
         </div>
-        </header>
       </div>
     );
   }
