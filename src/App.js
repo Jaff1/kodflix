1a44afb1f38cedebf4ef7eaf9625bc460e9d8387
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-11 13:14:36 +0000

    Introduce routing / navigation #8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Gallery from './Gallery';
+import Details from './Details';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Gallery />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={Gallery} />
+          <Route exact path='/details' component={Details} />
+        </div>
+      </Router>
     );
   }
 }
--- src/TVShow.js
+++ src/TVShow.js
@@ -1,10 +1,12 @@
 import React from 'react';
-export default function TVShow (props) {
-    return (<div class="item">
-    <img src={props.cover}  className="tVCover" alt='' />
-    <div className='overlay'>
-      <h1>{props.name}</h1>
-    </div>
-    </div>)
-    }
-    
+import { Link } from 'react-router-dom';
+
+export default function TVShow(props) {
+    return (
+        <Link to='/details' class="item">
+            <img src={props.cover} className="tVCover" alt='' />
+            <div className='overlay'>
+                <h1>{props.name}</h1>
+            </div>
+        </Link>)
+}
