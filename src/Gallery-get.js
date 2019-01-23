90c5bba8c396ba3254b48445f11a24cf1de54f8f
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-23 14:32:52 +0000

    Move the covers data model to a proper front-end data source #12  - hase some warnings re Class Vs ClassName. will sort later

---
+++ src/Gallery-get.js
@@ -0,0 +1,17 @@
+import blackMirror from './black-mirror-tv-show.jpg';
+import breakingBad from './BreakingBad.jpg';
+import deathNote from './lightdeathnoteposter.jpg';
+import gameOfThrones from './GameOfThrones.jpg';
+import walkingDead from './The-Walking-Dead-Season-3.jpg';
+import wire from './TheWire.jpg';
+
+export default function getGallery() {
+    return [
+        { id: 'blackMirror', name: 'Black Mirror', cover: blackMirror },
+        { id: 'breakingBad', name: 'Breaking Bad', cover: breakingBad },
+        { id: 'deathNote', name: 'Death Note', cover: deathNote }, 
+        { id: 'gameofThrones', name: 'Game of Thrones', cover: gameOfThrones },
+        { id: 'deadWalkers', name: 'Dead Walkers', cover: walkingDead },
+        { id: 'wire', name: 'Wire', cover: wire }
+    ]
+}
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import TVShow from './TVShow';
-
-import blackMirror from './black-mirror-tv-show.jpg';
-import breakingBad from './BreakingBad.jpg';
-import deathNote from './lightdeathnoteposter.jpg';
-import gameOfThrones from './GameOfThrones.jpg';
-import walkingDead from './The-Walking-Dead-Season-3.jpg';
-import wire from './TheWire.jpg';
+import getGallery from './Gallery-get';
 
 export default function Gallery(props) {
     return (
     <div>
         <div class="container">
-            <TVShow id = 'blackMirror' name='Black Mirror' cover={blackMirror} />
-            <TVShow id = 'breakingBad' name='Breaking Bad' cover={breakingBad} />
-            <TVShow id = 'deathNote' name='Death Note' cover={deathNote} />
-            <TVShow id = 'gameofThrones' name='Game of Thrones' cover={gameOfThrones} />
-            <TVShow id = 'deadWalkers' name='Dead Walkers' cover={walkingDead} />
-            <TVShow id = 'wire' name='Wire' cover={wire} />
+        {
+            getGallery().map(tVShow => (
+                <TVShow 
+                  key={tVShow.id} 
+                  id={tVShow.id} 
+                  name={tVShow.name} 
+                  cover={tVShow.cover} />
+            ))
+        }
         </div>
     </div>
     )
 }
+
