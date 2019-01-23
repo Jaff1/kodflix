f64691f977abd384c175365cea380ea89ed43b93
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-23 13:30:48 +0000

    Renaming TVshow image aliases

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import TVShow from './TVShow';
 
-import tVCoverShow1 from './black-mirror-tv-show.jpg';
-import tVCoverShow2 from './BreakingBad.jpg';
-import tVCoverShow3 from './lightdeathnoteposter.jpg';
-import tVCoverShow4 from './GameOfThrones.jpg';
-import tVCoverShow5 from './The-Walking-Dead-Season-3.jpg';
-import tVCoverShow6 from './TheWire.jpg';
+import blackMirror from './black-mirror-tv-show.jpg';
+import breakingBad from './BreakingBad.jpg';
+import deathNote from './lightdeathnoteposter.jpg';
+import gameOfThrones from './GameOfThrones.jpg';
+import walkingDead from './The-Walking-Dead-Season-3.jpg';
+import wire from './TheWire.jpg';
 
 export default function Gallery(props) {
     return (
     <div>
         <div class="container">
-            <TVShow id = 'blackMirror' name='Black Mirror' cover={tVCoverShow1} />
-            <TVShow id = 'breakingBad' name='Breaking Bad' cover={tVCoverShow2} />
-            <TVShow id = 'deathNote' name='Death Note' cover={tVCoverShow3} />
-            <TVShow id = 'gameofThrones' name='Game of Thrones' cover={tVCoverShow4} />
-            <TVShow id = 'deadWalkers' name='Dead Walkers' cover={tVCoverShow5} />
-            <TVShow id = 'wire' name='Wire' cover={tVCoverShow6} />
+            <TVShow id = 'blackMirror' name='Black Mirror' cover={blackMirror} />
+            <TVShow id = 'breakingBad' name='Breaking Bad' cover={breakingBad} />
+            <TVShow id = 'deathNote' name='Death Note' cover={deathNote} />
+            <TVShow id = 'gameofThrones' name='Game of Thrones' cover={gameOfThrones} />
+            <TVShow id = 'deadWalkers' name='Dead Walkers' cover={walkingDead} />
+            <TVShow id = 'wire' name='Wire' cover={wire} />
         </div>
     </div>
     )
