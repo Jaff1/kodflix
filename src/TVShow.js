1d258af3794a231cbcd0105703087b5d07b165ee
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-25 15:48:40 +0000

    class Vs className fixes in Gallery.js and TVShow.js
    in React cannot use the `class` - it conflicts with `class` in JavaScript

--- src/TVShow.js
+++ src/TVShow.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function TVShow(props) {
     return (
-        <Link to={`/${props.id}`} class="item">
+        <Link to={`/${props.id}`} className="item">
             <img src={props.cover} className="tVCover" alt='' />
             <div className='overlay'>
                 <h1>{props.name}</h1>
