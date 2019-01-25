3648952fff4d68b57f96d14cf01aaba8573c77ee
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-25 13:04:17 +0000

    Introduce component state #13

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,30 @@
-import React from 'react';
+
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details() {
-    return (
-        <div><h1>Welcome to the details page, WIP;-)</h1>
-        <Link to='/'>Back to home page</Link>
-        </div>
-    )
+export default class Details extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            welcomeMessage: 'Welcome to the details page, WIP!!!;-)>'
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                welcomeMessage: 'The best lies ahead'
+            });
+        }, 3000);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.welcomeMessage}</h1>
+                <Link to='/'>Back to home page</Link>
+            </div>
+        );
+    }
 }
