bda6f34f6d84be3629b19a52d2bb8947ef8b9158
Munawar Jaffer <munawar.jaffer@googlemail.com>
2019-01-28 17:15:44 +0000

    Show the TV show title on the details page #14

--- src/Details.js
+++ src/Details.js
@@ -1,28 +1,28 @@
 
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import getGallery from './Gallery-get';
 
 export default class Details extends Component {
 
     constructor() {
         super();
         this.state = {
-            welcomeMessage: 'Welcome to the details page, WIP!!!;-)>'
+            show: {}
         };
     }
 
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                welcomeMessage: 'The best lies ahead'
-            });
-        }, 3000);
+        let showId = this.props.match.params.showId; 
+        let show = getGallery()
+            .find((show) => show.id === showId);
+        this.setState({ show });        
     }
 
     render() {
         return (
             <div>
-                <h1>{this.state.welcomeMessage}</h1>
+                <h1>{this.state.show.name}</h1>
                 <Link to='/'>Back to home page</Link>
             </div>
         );
