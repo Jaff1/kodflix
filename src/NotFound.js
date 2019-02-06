1e70d730007c8808dfec09ca16c4037860db582a
Munawar <munawar.jaffer@googlemail.com>
2019-02-06 00:46:46 +0000

    added a "Back to home page" link is nor found

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
     return (
         <div>
             <h1> oooooops....The page doesn't exist :-( </h1>
+            <Link to='/'>Back to home page</Link>
         </div>
     )
 }
