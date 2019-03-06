6babd2e5f71cea27eb865a86619ad34fd319f15a
Munawar <munawar.jaffer@googlemail.com>
2019-03-06 13:54:34 +0000

    Enable the backend server in prod #23 - add npm-run-all as prod dependen

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = process.env.PORT || 3001
-const path = require('path');
 
 // Backend Server provides: id, title, synopsis
 // Frontend expects id name details
