6f6991c0438bda09af82e8351ac4f0d149a09a8b
Munawar <munawar.jaffer@googlemail.com>
2019-03-18 15:29:09 +0000

    Replace the TV show frontend data source, with the backend one #24 - use sone static data for State in Galley

--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -7,7 +7,24 @@ export default class Gallery extends Component {
 	constructor() {
 		super();
 		this.state = {
-			loadingMessage: 'Loading JafFlix show data dudes'
+			loadingMessage: 'Loading JafFlix show data dudes',
+			shows: [
+				{
+					id: 'black-mirror',
+					title: 'Black Mirror',
+					synopsis: 'An anthology series exploring a twisted, high-tech world where humanity\'s greatest innovations and darkest instincts collide.'
+				},
+				{
+					id: 'breakingBad',
+					title: 'Breaking Bad',
+					synopsis: 'A once loyal father and chemistry teacher, Walter White, turns to a life of crime due to developing stage 3 terminal lung cancer. He is told he has two years to live. ... Breaking Bad follows protagonist Walter White, a chemistry teacher who lives in New Mexico with his wife and teenage son who has cerebral palsy.'
+				},
+				{
+					id: 'deathNote',
+					title: 'Death Note',
+					synopsis: 'Light Yagami (Tatsuya Fujiwara) is a normal, undistinguished college student -- that is, until he discovers an odd notebook lying on the ground. He soon discovers that the notebook has magic powers: If someone\'s name is written on it while the writer imagines that person\'s face, he or she will die. Intoxicated with his new godlike power, Light kills those he deems unworthy of life. But a mysterious detective known only as L (Ken\'ichi Matsuyama) becomes determined to put a stop to his reign.'
+				},
+			]
 		};
 	}
 
@@ -25,7 +42,7 @@ export default class Gallery extends Component {
 				<div className="container">
 					<h1>{this.state.loadingMessage}</h1>
 					{
-						getGallery().map(tVShow => (
+						this.state.shows.map(tVShow => (
 							<TVShow
 								key={tVShow.id}
 								id={tVShow.id}
