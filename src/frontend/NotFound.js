ef37de84b6181b3251f0f4cfc8c41758dbb0051f
Munawar <munawar.jaffer@googlemail.com>
2019-03-20 14:46:33 +0000

    elegance/lint changes

--- src/frontend/NotFound.js
+++ src/frontend/NotFound.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function NotFound() {
-    return (
-        <div>
-            <h1> oooooops....The page doesn't exist :-( </h1>
-            <Link to='/'>Back to home page</Link>
-        </div>
-    )
+	return (
+		<div>
+			<h1> oooooops....The page doesn't exist :-( </h1>
+			<Link to='/'>Back to home page</Link>
+		</div>
+	);
 }
